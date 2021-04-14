8b08e0781de35b549cf5729ad855ebe5caff4698
SprengerV <b.francis@sprengerv.com>
2021-04-13 22:13:20 -0400

    server file and base routes structure

---
+++ server.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const routes = require('./controllers');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
+
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}).then(mongoData => {
+    console.log(mongoData);
+    app.listen(PORT, () => {
+        console.log(`APP LISTENING ON PORT ${PORT}`);
+    });
+});
