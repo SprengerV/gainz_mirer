73f47f8f9f47c085789f5b28356567f86dc22a81
SprengerV <b.francis@sprengerv.com>
2021-04-16 11:54:20 -0400

    outlined which api routes I need

---
+++ controllers/api/workoutRoutes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+
+router.get('/workouts', (req, res) => {
+
+});
+router.put('/workouts', (req, res) => {
+
+});
+router.post('/workouts', (req, res) => {
+
+});
+router.get('workouts/range', (req, res) => {
+
+});
+
+module.exports = router;
